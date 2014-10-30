5b09ff73acfe6a07dde32bcd81540fedc8b6def8
rupy <rupyapps@gmail.com>
2014-10-31 06:10:20 +0900

    引数にtrainとtestを受けるように修正

--- features_libsvm_format.sh
+++ features_libsvm_format.sh
@@ -6,9 +6,31 @@ do
 		echo "$d is not directory" 1>&2
 		continue
 	fi
-		
+	
+	count=$(( `ls -1 $d | wc -l` ))
+	if [ $1 = train ]
+	then
+		max=$count
+		min=$(( count / 2 ))
+	else
+		max=$(( count / 2 ))
+		min=0
+	fi
+	#echo $count
+	#echo $max
+	#echo $min
 	for f in "$d"/*
 	do
+		if [ $count -eq $min ]
+		then
+			break
+		elif [ $count -gt $max ] 
+		then
+			count=$(( count - 1 ))
+			continue
+		fi
+		#echo $count
+		count=$(( count - 1 ))
 		echo -n "$category_num"" "
 		if [ ${f##*.} != jpg ]
 		then
@@ -16,7 +38,7 @@ do
 			continue
 		fi
 		# echo "$f"
-		python feature.py ${f} 2> /dev/null
+		python caffe_script/feature.py ${f} 2> /dev/null
 	done
-	category_num=`expr $category_num + 1`
+	category_num=$(( category_num + 1 ))
 done
